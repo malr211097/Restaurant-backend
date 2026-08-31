08315b278b98f4863b6fb13fd3ed2a9f571de848
malr211097 <202030442+malr211097@users.noreply.github.com>
2026-08-31 00:35:39 -0500

    Add Express server setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json()) 
+
+app.get('/', (req, res) => {
+    res.send('Server is running successfully')
+
+    })
+
+    app.listen(PORT, () => {
+       console.log(`Server runing at http://localhost:${PORT}`)  
+})
+
+  
